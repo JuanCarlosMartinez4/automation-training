9cd566632063b29f65c8f11a48865449dbbd32a0
JuanCarlosMartinez4 <Juan.Martinez@fundacion-jala.org>
2020-05-03 18:16:22 -0400

    improved headless

--- project/cucumber/features/support/env.rb
+++ project/cucumber/features/support/env.rb
@@ -31,9 +31,8 @@ AfterConfiguration do
   end
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu] }
+      chromeOptions: { args: %w['headless', 'disable-gpu', 'no-sandbox'] }
     )
-
     Capybara::Selenium::Driver.new app,
                                    browser: :chrome,
                                    desired_capabilities: capabilities
